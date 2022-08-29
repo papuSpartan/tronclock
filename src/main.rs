9af7c046ea425de4ed895beda448226452aa44b6
papuSpartan <mcgpapu@gmail.com>
2022-08-29 00:48:07 -0500

    refactor to use crossterm instead of ncurses to support resizing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
-use ncurses::*;
 use chrono::Utc;
 use std::{thread, time::Duration, process};
+use std::io::{stdout, Write};
 use ctrlc;
+use crossterm::{
+    ExecutableCommand, terminal::{size, SetSize}, execute, queue,
+    terminal, cursor, style::{Print}
+};
+
 
 fn main() {
-    static WIN_HEIGHT: i32 = 0;
-    static WIN_WIDTH: i32 = 0;
-    let mut max_x: i32 = 0;
-    let mut max_y: i32 = 0; 
-
     //setup ctrl-c handler
     //calling endwin() here ensures that terminal still has a cursor after exit
-    ctrlc::set_handler(|| {endwin(); process::exit(0)} ).expect("couldn't set ctrl-c handler");
+    ctrlc::set_handler(|| {
+        terminal::Clear(terminal::ClearType::All); 
+        process::exit(0)
+    }).expect("couldn't set ctrl-c handler");
 
-    //start ncurses
-    initscr();
-    curs_set(ncurses::CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    //start crossterm
+    let mut stdout = stdout();
+    //prepare terminal
+    stdout.execute(terminal::Clear(terminal::ClearType::All)).expect("Could not clear terminal");
+    stdout.execute(cursor::Hide).unwrap();
 
+    stdout.flush().expect("Unable to flush to terminal");
 
-    //get screen bounds
-    getmaxyx(stdscr(), &mut max_y, &mut max_x);
-    let row = (max_y - WIN_HEIGHT) /2;
-    let col = (max_x - WIN_WIDTH) /2;
-
-
+    let mut cur_size: (u16, u16) = size().unwrap();
     loop {
-        clear();
+
+        //handle terminal resizing
+        let new_size = size().unwrap();
+        if !(new_size == cur_size) {
+            let (height, width) = new_size;
+
+            cur_size = new_size;
+            SetSize(height, width);
+            stdout.execute(terminal::Clear(terminal::ClearType::All)).expect("Could not clear terminal");
+        }
+
+
+        //get the date string we will be using to display to the center of the screen
         let date_time = Utc::now();
         //formatting https://docs.rs/chrono/latest/chrono/format/strftime/index.html#fn1
         let formatted_time = date_time.format("%y:%m:%d:%T:%f").to_string();
@@ -33,10 +46,19 @@ fn main() {
         //get formatted date-time string with the unneccesary digits of precision thrown off
         let (out_string, _extra) = formatted_time.split_at(ft_len - 7);
 
-        //move to middle of ncurses window within screen while adjusting for date-time string size
-        mvprintw(row, col - (ft_len as i32 / 2), out_string);
-        refresh();
-        thread::sleep(Duration::from_millis(25));
+        //have to make sure to adjust for the output string's length here so it is actually cent.
+        let (height, width) = cur_size;
+        let mid_x = width / 2;
+        let mid_y = height / 2;
+        queue!(stdout, cursor::MoveTo(mid_y - ((out_string.len() as u16) / 2), mid_x))
+            .expect("Could not move terminal cursor");
+        execute!(stdout, Print(out_string)).expect("Could not place text");
+
+        //refresh output here
+        stdout.flush().expect("Unable to flush to terminal");
+
+        //delay so that clock doesn't look like it's going too fast
+        thread::sleep(Duration::from_millis(35));
     }
 
 }
